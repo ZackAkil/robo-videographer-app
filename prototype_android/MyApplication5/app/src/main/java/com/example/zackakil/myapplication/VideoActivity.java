441c6771bae0bc22f0b15b6de0b20aebc892af8d
ZackAkil <zackakil94@gmail.com>
2017-12-26 18:53:43 +0000

    [broken] but resizing bitmap to fit tensor input shape

--- prototype_android/MyApplication5/app/src/main/java/com/example/zackakil/myapplication/VideoActivity.java
+++ prototype_android/MyApplication5/app/src/main/java/com/example/zackakil/myapplication/VideoActivity.java
@@ -174,6 +174,10 @@ public class VideoActivity extends AppCompatActivity {
                             final int b = Color.blue(rgbFrameBitmap.getPixel(0, 0));
 
 
+                            float prediction = 0;
+                            prediction = getPredictionFromTf();
+
+                            Log.d("My App", "prediction ="+ prediction);
 //                        Log.d("My App", "new counts");
 //                        final int avgY = getAverageFromBuffer(image.getPlanes()[0].getBuffer());
 //                        Log.d("My App", "sum ="+ avgY);
@@ -274,6 +278,31 @@ public class VideoActivity extends AppCompatActivity {
         }else{
             nTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
         }
+    }
+
+    private float getPredictionFromTf(){
+
+        final int[] pixels = new int[80*640];
+        Log.d("My App", "width ="+ rgbFrameBitmap.getWidth());
+        Log.d("My App", "height ="+ rgbFrameBitmap.getHeight());
+
+        Bitmap newRgbFrameBitmap = null;
+
+        newRgbFrameBitmap = Bitmap.createScaledBitmap(rgbFrameBitmap, 640, 80, false);
+
+        newRgbFrameBitmap.getPixels (pixels, 0, newRgbFrameBitmap.getWidth(), 0, 0, 640, 80);
+
+        tfHelper.feed("conv2d_6_input", pixels);
+
+        String[] outputNames = new String[] { "dense_15/BiasAdd" };
+
+        tfHelper.run(outputNames);
+
+        float[] output = new float[1];
+
+        tfHelper.fetch("dense_15/BiasAdd", output);
+
+        return output[0];
     }
 
     private void setupCamera(int width, int height){
@@ -450,43 +479,4 @@ public class VideoActivity extends AppCompatActivity {
         }
     }
 
-
-    //  Byte decoder : ---------------------------------------------------------------------
-    void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height) {
-        // Pulled directly from:
-        // http://ketai.googlecode.com/svn/trunk/ketai/src/edu/uic/ketai/inputService/KetaiCamera.java
-        final int frameSize = width * height;
-
-        for (int j = 0, yp = 0; j < height; j++) {       int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
-            for (int i = 0; i < width; i++, yp++) {
-                int y = (0xff & ((int) yuv420sp[yp])) - 16;
-                if (y < 0)
-                    y = 0;
-                if ((i & 1) == 0) {
-                    v = (0xff & yuv420sp[uvp++]) - 128;
-                    u = (0xff & yuv420sp[uvp++]) - 128;
-                }
-
-                int y1192 = 1192 * y;
-                int r = (y1192 + 1634 * v);
-                int g = (y1192 - 833 * v - 400 * u);
-                int b = (y1192 + 2066 * u);
-
-                if (r < 0)
-                    r = 0;
-                else if (r > 262143)
-                    r = 262143;
-                if (g < 0)
-                    g = 0;
-                else if (g > 262143)
-                    g = 262143;
-                if (b < 0)
-                    b = 0;
-                else if (b > 262143)
-                    b = 262143;
-
-                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
-            }
-        }
-    }
 }
