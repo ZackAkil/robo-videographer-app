d76500c329c22b66febdc8a4ee8a2cd163fcf3b6
ZackAkil <zackakil94@gmail.com>
2017-12-31 17:08:10 +0000

    added more filter functions and more properties

--- prototype_android/MyApplication5/app/src/main/java/com/example/zackakil/myapplication/ImagePreProcessor.java
+++ prototype_android/MyApplication5/app/src/main/java/com/example/zackakil/myapplication/ImagePreProcessor.java
@@ -2,9 +2,11 @@ package com.example.zackakil.myapplication;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.util.Log;
 
 /**
  * Created by zackakil on 31/12/2017.
@@ -14,15 +16,37 @@ public class ImagePreProcessor {
 
     private float[] previousFrame;
     private int[] currentFramePixels;
+    private float[] currentFrameFloatPixels;
+    private float[] deltaPixels;
+    private int frameSize;
+    private int frameHeight;
+    private  int frameWidth;
 
 
-    ImagePreProcessor(int frameSize){
-        this.previousFrame = new float[frameSize];
+    ImagePreProcessor(int frameWidth, int frameHeight){
+
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+
+        this.frameSize = this.frameWidth * this.frameHeight;
+        this.previousFrame = new float[this.frameSize];
+        this.deltaPixels = new float[this.frameSize];
+        this.currentFrameFloatPixels = new float[this.frameSize];
     }
-    
+
 
     public void feedFrame(Bitmap frame){
 
+        final Bitmap newRgbFrameBitmap = Bitmap.createScaledBitmap(
+                                                ImagePreProcessor.toGrayscale(frame),
+                                                this.frameWidth, this.frameHeight, false);
+
+        newRgbFrameBitmap.getPixels(this.currentFramePixels, 0, this.frameWidth, 0, 0,
+                                        this.frameWidth, this.frameHeight);
+
+        for(int i = 0; i<80*640; i++){
+            this.currentFrameFloatPixels[i] = ((float) Color.red(this.currentFramePixels[i])) / 255.f ;
+        }
     }
 
     public float[] getLatestDeltaFrame(){
@@ -45,5 +69,30 @@ public class ImagePreProcessor {
         paint.setColorFilter(f);
         c.drawBitmap(bmpOriginal, 0, 0, paint);
         return bmpGrayscale;
+    }
+
+    private float[] getDeltaPixels(float[] frame1, float[] frame2){
+
+
+        float sum = 0;
+        for(int i = 0; i<this.frameSize; i++){
+            this.deltaPixels[i] = Math.max(frame1[i] - frame2[i], 0);
+            sum += Math.max(frame1[i] - frame2[i], 0);
+        }
+
+        Log.d("My App", "sum ="+ sum);
+        return this.deltaPixels;
+    }
+
+    public static int [] floatArray2intArray (float[] values)
+    {
+        int[] out = new int[values.length];
+
+        for (int i =0; i<values.length; i++){
+            int c =  (int) Math.min(values[i]*255, 255);
+            out[i] = Color.rgb(c, c, c);
+        }
+
+        return out;
     }
 }
