7f4c540cb440024b161ab45910589a70ca287c0e
ZackAkil <zackakil94@gmail.com>
2018-07-07 15:06:02 +0100

    play till end gracefully

--- video_processing/image_tagger_gui.py
+++ video_processing/image_tagger_gui.py
@@ -10,12 +10,16 @@ import skvideo.io
 import skvideo.datasets
 
 video_file_name = '../video/raw/VID_20180325_143338.mp4'
-
 video_data = skvideo.io.vread(video_file_name)
 
+print('video data ', video_data.shape)
+
+x,y = 0,0
+image_index = 0
+stop = True
+speed = 500
+
 class Application(Frame):
-    def say_hi(self):
-        print("hi there, everyone!")
 
     def createWidgets(self):
         self.QUIT = Button(self)
@@ -25,98 +29,67 @@ class Application(Frame):
 
         self.QUIT.pack({"side": "left"})
 
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
-
-        self.hi_there.pack({"side": "left"})
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
 
-x,y = 0,0
+
 
 def motion(event):
-
     global x, y
     x, y = event.x, event.y
-    # w.delete("all")
-    # w.create_image(0, 0, anchor=tkinter.NW, image=current_photo)
-    # w.create_line(event.x, 0, event.x, 450,fill="red")
-    # display_photo_label_line()
 
-    # print('{}, {}'.format(x, y))
-
-    
 
 def next_image():
-    global image_index, x, y, stop
+    global image_index, x, y, stop, speed
 
     display_image(image_index)
-    image_index+=5
+    image_index+=1
     w.delete("all")
     w.create_image(0, 0, anchor=tkinter.NW, image=current_photo)
     w.create_line(x, 0, x, 450,fill="red")
     w.create_line(0, y, 650, y,fill="red")
-    if not stop:
-        w.after(500, next_image)
+    print(x,y)
 
-def display_photo_label_line():
-    pass
-    # photo_line_pos = ilc.get_current_pos_value()
-    # if photo_line_pos:
-    #     w.create_line(photo_line_pos, 0, photo_line_pos, 450,fill="blue")
+    if image_index >= len(video_data):
+        print("finished data")
+    elif not stop:
+        w.after(speed, next_image)
 
-image_index = 0
 def play(event):
     global stop 
-    stop = False
-    next_image()
+    if stop:
+        stop = False
+        next_image()
     
-    pass
 
 
-
-    # ilc.set_current_pos_value(event.x)
-    # display_photo_label_line()
-    # ilc.next_photo()
-    # display_image(ilc.current_image_name)
-    # print ("clicked at", event.x, event.y)
-    # ilc.save_labels(LABEL_CSV)
-stop = False
 def pause(event):
     global stop 
     stop = True
-    pass
-    # ilc.set_current_pos_value(None)
-    # display_photo_label_line()
-    # ilc.next_photo()
-    # display_image(ilc.current_image_name)
-    # print ("dump clicked at", event.x, event.y)
-    # ilc.save_labels(LABEL_CSV)
+
 
 def display_image(image_index):
-
-    photo = ImageTk.PhotoImage(Image.fromarray(video_data[image_index].astype('uint8'), 'RGB'))
-    print(type(photo))
-    w.delete("all")
-    w.create_image(0, 0, anchor=tkinter.NW, image=photo)
     global current_photo
-    current_photo = photo
-
+    current_photo = ImageTk.PhotoImage(Image.fromarray(video_data[image_index].astype('uint8'), 'RGB'))
+ 
 def key(event):
+    global speed 
+
     if event.char == '\uf703':
         print('right')
-        ilc.next_photo()
-        display_image(ilc.current_image_name)
+        speed = max(speed-50, 10)
+
+
     elif event.char == '\uf702':
-        ilc.next_photo(-1)
-        display_image(ilc.current_image_name)
         print('left')
+        speed += 50
+
     elif event.char == '\uf701':
         print('down')
+
     else:
         print("pressed", repr(event.char))
 
@@ -124,19 +97,14 @@ def key(event):
 root = Tk()
 
 current_photo = None
-# mouse_line = None
-# photo_line = None
 
-w = Canvas(root, width=650, height=500)
+w = Canvas(root, width=1000, height=1000)
 w.pack()
 
 w.bind('<Motion>', motion)
 w.bind("<Button-1>", play)
 w.bind("<Button-2>", pause)
-# root.bind("<Key>", key)
-
-
-
+root.bind("<Key>", key)
 
 
 app = Application(master=root)
